bbddd7f86df5153d7858663bf23b56762ad139ce
marcus-medeiros <marcus.vinicius.medeiros@ee.ufcg.edu.br>
2025-08-09 22:22:53 -0300

    Adicionando arquivos de BMS

--- arquivos.py
+++ arquivos.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from streamlit_option_menu import option_menu
+
 
 def app ():
     st.header("Aplicações e Análise Gráfica")
